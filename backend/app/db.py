e156ced865f38758333bdd9b7ba1109b7f245f5c
TheJokerGun <noahdom@web.de>
2025-09-12 12:16:42 +0200

    changed motor to pymongo

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,14 +1,11 @@
 # backend/app/db.py
 import os
-from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import MongoClient
+from pymongo.mongo_client import AsyncMongoClient
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 DB_NAME = os.getenv("MONGO_DB", "feedbackdb")
 
-client = AsyncIOMotorClient(MONGO_URI)
+client = AsyncMongoClient(MONGO_URI)
 db = client[DB_NAME]
 feedback_collection = db["feedbacks"]
-
-# optional create indexes at startup
-async def ensure_indexes():
-    await feedback_collection.create_index("timestamp")
